23b7ab8183113220d9258671ccf368aaa67b0e77
luyannafm <luyannafm@gmail.com>
2023-05-02 17:30:58 -0300

    adiciona arquivos index e scripts

---
+++ 01-fundamentos/sec-03-introducao-javascript/dia-01-js-primeiros/scripts.js
@@ -0,0 +1,5 @@
+let nota = 70
+
+if (nota >= 80) {console.log("parabens, você faz parte do grupo de pessoas aprovadas");} 
+else if (nota < 80 && nota >= 60) {console.log("você esta na nossa lista de espera");} 
+else if (nota < 60) {console.log("infelizmente, você reprovou");} 
